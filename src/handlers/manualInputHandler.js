7da36b1dde2d11655bd304b5b518ffd100e78f1a
Urvish <urathi2005@gmail.com>
2025-07-16 14:12:35 -0600

    Ensure location set on manual auto location get

--- src/handlers/manualInputHandler.js
+++ src/handlers/manualInputHandler.js
@@ -1,6 +1,7 @@
 import { handleLocationSuccess } from "./locationHandlers";
 
-export async function handleCitySelect(city, setAppState, setWeatherData) {
+export async function handleCitySelect(city, setAppState, setWeatherData, setLocation) {
     const coords = {latitude: city.latitude, longitude: city.longitude}
+    setLocation(coords);
     await handleLocationSuccess(coords, setAppState, setWeatherData);
 }
